6c3b8dac9d80ddbfe69a9bfa6e65c8d769b3861a
JanSemorad <JanSemorad@users.noreply.github.com>
2021-03-16 18:26:15 +0100

    Add files via upload

--- www/code/ReportTemplate/js/loadcss.js
+++ www/code/ReportTemplate/js/loadcss.js
@@ -3,3 +3,4 @@
 	var css2 = '<link rel="stylesheet" href="https://jansemorad.github.io/www/code/ReportTemplate/css/bootstrap.min.css">'
 	var css3 = '<link href="https://jansemorad.github.io/www/code/ReportTemplate/css//BootSideMenu.css" rel="stylesheet">'
 	var css = css1 + css2 + css3
+	document.write(css)
