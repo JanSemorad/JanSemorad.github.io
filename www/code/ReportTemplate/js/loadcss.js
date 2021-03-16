41596f52af515a31fdf16ece371f683e107af117
JanSemorad <JanSemorad@users.noreply.github.com>
2021-03-16 18:20:40 +0100

    Add files via upload

---
+++ www/code/ReportTemplate/js/loadcss.js
@@ -0,0 +1,5 @@
+﻿	var css1 = '<link href="https://jansemorad.github.io/www/code/ReportTemplate/css/jquerysctipttop.css" rel="stylesheet" type="text/css">'
+  	// <!-- Bootstrap -->
+	var css2 = '<link rel="stylesheet" href="https://jansemorad.github.io/www/code/ReportTemplate/css/bootstrap.min.css">'
+	var css3 = '<link href="https://jansemorad.github.io/www/code/ReportTemplate/css//BootSideMenu.css" rel="stylesheet">'
+	var css = css1 + css2 + css3
